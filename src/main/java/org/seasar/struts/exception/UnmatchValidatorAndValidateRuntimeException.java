1c66c02765f52d4a47fd11b14893fa0dd870e5ea
Yasuo Higa <higayasuo@gmail.com>
2008-05-16 08:41:35 +0000

    [SASTRUTS-47]@Executeのvalidate要素で複数の検証メソッドを指定できるようにしました。
    git-svn-id: https://www.seasar.org/svn/sastruts/trunk/sa-struts@499 311f1561-e253-4dfb-a9fb-882cde7d7359

---
+++ src/main/java/org/seasar/struts/exception/UnmatchValidatorAndValidateRuntimeException.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2004-2008 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.struts.exception;
+
+import org.seasar.framework.exception.SRuntimeException;
+
+/**
+ * validate属性でアットマークが指定されたのにvalidator属性がfalseの場合の例外です。
+ * 
+ * @author higa
+ * 
+ */
+public class UnmatchValidatorAndValidateRuntimeException extends
+        SRuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    private Class<?> actionClass;
+
+    private String executeMethodName;
+
+    /**
+     * インスタンスを構築します。
+     * 
+     * @param actionClass
+     *            アクションクラス
+     * @param executeMethodName
+     *            実行メソッド名
+     */
+    public UnmatchValidatorAndValidateRuntimeException(Class<?> actionClass,
+            String executeMethodName) {
+        super("ESAS0011", new Object[] { actionClass.getName(),
+                executeMethodName });
+        this.actionClass = actionClass;
+        this.executeMethodName = executeMethodName;
+    }
+
+    /**
+     * アクションクラスを返します。
+     * 
+     * @return アクションクラス
+     */
+    public Class<?> getActionClass() {
+        return actionClass;
+    }
+
+    /**
+     * 実行メソッド名を返します。
+     * 
+     * @return 実行メソッド名
+     */
+    public String getExecuteMethodName() {
+        return executeMethodName;
+    }
+}
