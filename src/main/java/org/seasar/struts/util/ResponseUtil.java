13e70871ceb8af5ba6e28fad8727b790acc6c984
Yasuo Higa <higayasuo@gmail.com>
2008-07-07 09:11:08 +0000

    [SASTRUTS-79]Ajaxのサンプルを追加しました。
    git-svn-id: https://www.seasar.org/svn/sastruts/trunk/sa-struts@636 311f1561-e253-4dfb-a9fb-882cde7d7359

--- src/main/java/org/seasar/struts/util/ResponseUtil.java
+++ src/main/java/org/seasar/struts/util/ResponseUtil.java
@@ -17,6 +17,8 @@ package org.seasar.struts.util;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -62,6 +64,67 @@ public final class ResponseUtil {
                 out.write(data);
             } finally {
                 out.close();
+            }
+        } catch (IOException e) {
+            throw new IORuntimeException(e);
+        }
+    }
+
+    /**
+     * レスポンスにテキストを書き込みます。
+     * 
+     * @param text
+     *            テキスト
+     */
+    public static void write(String text) {
+        write(text, null, null);
+    }
+
+    /**
+     * レスポンスにテキストを書き込みます。
+     * 
+     * @param text
+     *            テキスト
+     * @param contentType
+     *            コンテントタイプ。 デフォルトはtext/plain。
+     */
+    public static void write(String text, String contentType) {
+        write(text, contentType, null);
+    }
+
+    /**
+     * レスポンスにテキストを書き込みます。
+     * 
+     * @param text
+     *            テキスト
+     * @param contentType
+     *            コンテントタイプ。 デフォルトはtext/plain。
+     * @param encoding
+     *            エンコーディング。 指定しなかった場合は、リクエストのcharsetEncodingが設定される。
+     *            リクエストのcharsetEncodingも指定がない場合は、UTF-8。
+     */
+    public static void write(String text, String contentType, String encoding) {
+        if (contentType == null) {
+            contentType = "text/plain";
+        }
+        if (encoding == null) {
+            encoding = RequestUtil.getRequest().getCharacterEncoding();
+            if (encoding == null) {
+                encoding = "UTF-8";
+            }
+        }
+        HttpServletResponse response = getResponse();
+        response.setContentType(contentType + "; charset=" + encoding);
+        try {
+            PrintWriter out = null;
+            try {
+                out = new PrintWriter(new OutputStreamWriter(response
+                        .getOutputStream(), encoding));
+                out.print(text);
+            } finally {
+                if (out != null) {
+                    out.close();
+                }
             }
         } catch (IOException e) {
             throw new IORuntimeException(e);
